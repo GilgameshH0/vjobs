2ec509a4a85999b143e6d56320708a0c6385427a
o.shilkin <oleg.miracle@yandex.ru>
2021-12-13 20:41:29 +0300

    Initial commit

---
+++ src/main/java/com/miracle/vjobs/VjobsApplication.java
@@ -0,0 +1,13 @@
+package com.miracle.vjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VjobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VjobsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miracle/vjobs/VjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miracle.vjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VjobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
